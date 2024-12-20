042e1f36128ac48dad3b58499ad061de483cfd23
harshid9898 <harshidthakor385@gamil.com>
2024-12-20 11:33:08 +0530

    add js file

---
+++ js/button.js
@@ -0,0 +1,16 @@
+$(window).scroll(function () {
+    if ($(this).scrollTop() >= 100) {
+      $(".btntop").fadeIn(200);
+    } else {
+      $(".btntop").fadeOut(200);
+    }
+  });
+  $(".btntop").click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      500
+    );
+  });
+
